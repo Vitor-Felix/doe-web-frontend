9978dbc79638a95b7f2089739b037e559e1a2790
Vitor Felix <vytorfelix@gmail.com>
2025-07-04 20:18:21 -0300

    DFR-1 🌱 Bootstrap React + Vite + TypeScript project. Fixes #1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-primary">Hello Doe</h1>
+      <p className="text-gray-600 mt-2">Welcome to your React + Tailwind project!</p>
+    </div>
+  );
+}
+
+export default App;
